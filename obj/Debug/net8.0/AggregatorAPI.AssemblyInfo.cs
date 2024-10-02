139188e06fc1ac322c41c440ce87114a771c5f52
bax082024 <bax082024@gmail.com>
2024-10-02 09:07:06 +0200

    added .

--- obj/Debug/net8.0/AggregatorAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/AggregatorAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AggregatorAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e9fe1c95e3b33e168c0f5a5d42201927d892c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("AggregatorAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AggregatorAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
