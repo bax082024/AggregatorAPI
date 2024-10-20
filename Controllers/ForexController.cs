e8d77da1139de9a6d4e12bcc746e3b7a7b926ecc
bax082024 <bax082024@gmail.com>
2024-10-20 21:47:38 +0200

    added forex controller

--- Controllers/ForexController.cs
+++ Controllers/ForexController.cs
@@ -1,8 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using AggregatorAPI.Services;
+using System.Reflection.Metadata.Ecma335;
 
 namespace AggregatorAPI.Controllers
 {
-  
+  [ApiController]
+  [Route("api/v1/forex")]
+  public class ForexController : ControllerBase
+  {
+    private readonly IForexService _forexService;
+
+    public ForexController(IForexService forexService)
+    {
+      _forexService = forexService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetForexRates([FromQuery] string currencies = "USD,EUR,GBP", [FromQuery] string source = "NOK")
+    {
+        var forexRates = await _forexService.GetForexRatesAsync(currencies, source);
+        return Ok(forexRates);
+    }
+  }
 }
